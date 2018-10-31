f1213841c3b0c8a4ac3de1281dda0f4c2c4fe5c0
Edward Kimber <contact@oberi.co.uk>
2018-10-31 12:23:41 +0000

    update clojure alpha dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject ekimber/clj-strava "0.1.1"
+(defproject ekimber/clj-strava "0.2.0-SNAPSHOT"
   :description "Strava API"
   :url "https://github.com/ekimber/clj-strava"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.16"]
-                 [org.clojure/data.json "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [http-kit "2.2.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [environ "1.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+                 [environ "1.1.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.4"]]
+                   :plugins [[lein-midje "3.0.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
